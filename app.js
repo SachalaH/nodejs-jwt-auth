5ec0d1bb78f5e68be92956a31e5c54756b37465a
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-08-14 21:36:38 +0530

    Initialized project and connected to mongo cluster

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI =
+  "mongodb+srv://stark:jwtStark1421@cluster0.smaoj.mongodb.net/jwt-auth";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
