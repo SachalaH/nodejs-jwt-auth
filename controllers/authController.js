79ca6799a82aa39220f808ef170e2effaaec64cb
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-08-15 13:25:11 +0530

    Made user model and tested creating a new user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,21 @@
+const User = require("../models/User");
+
 module.exports.getSignup = (req, res) => {
   res.render("signup");
 };
 module.exports.getLogin = (req, res) => {
   res.render("login");
 };
-module.exports.postSignup = (req, res) => {
+module.exports.postSignup = async (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);
-  res.send("New user signup");
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).send("Error, user creation failed.");
+  }
 };
-module.exports.postLogin = (req, res) => {
+module.exports.postLogin = async (req, res) => {
   const { email, password } = req.body;
   console.log(email, password);
   res.send("User login authentication");
