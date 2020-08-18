bd1b6324153e15c5292380ba2ebbb0457e456b9a
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-08-18 20:36:34 +0530

    Mongoose validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,24 @@
 const User = require("../models/User");
 
+// Error handling
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: "", password: "" };
+  // duplicate error code
+  if (err.code === 11000) {
+    errors.email = "That email is already registered";
+    return errors;
+  }
+  // validation errors
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      // console.log(properties);
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
+
 module.exports.getSignup = (req, res) => {
   res.render("signup");
 };
@@ -12,7 +31,8 @@ module.exports.postSignup = async (req, res) => {
     const user = await User.create({ email, password });
     res.status(201).json(user);
   } catch (error) {
-    res.status(400).send("Error, user creation failed.");
+    const errors = handleErrors(error);
+    res.status(400).json({ errors });
   }
 };
 module.exports.postLogin = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter an email"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "Please enter a valid email address"],
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6,
+    required: [true, "Please provide a password"],
+    minlength: [6, "Minimum length of password is 6"],
   },
 });
 
