d5ab4d05ceb41262414e96c84e081397166ead89
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-08-21 20:33:43 +0530

    Learnt about cookie parser and cookies

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cookieParser = require("cookie-parser");
 const app = express();
 const authRouter = require("./routes/authRouter");
 
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -27,3 +28,20 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRouter);
+
+// Testing cookie
+app.get("/set-cookie", (req, res) => {
+  // res.setHeader("Set-Cookie", "newUser=true");
+  res.cookie("newUser", false);
+  res.cookie("isEmployee", true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  });
+  res.send("You have been given a cookie");
+});
+
+app.get("/read-cookie", (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.json(cookies);
+});
